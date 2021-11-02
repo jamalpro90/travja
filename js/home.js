61acaad3f932879f69cea535131a6df5e8ff6a63
jamalpro90 <jamalpro90@gmail.com>
2021-11-03 03:33:16 +0700

    ganti logo brand, perbaiki footer, tambah hyperlink ke special offers

---
+++ js/home.js
@@ -0,0 +1,40 @@
+// const navbar = document.querySelector('.navbar');
+// const hero = document.querySelector('.hero');
+
+// const heroHeight = hero.clientHeight;
+// const navbarHeight = navbar.clientHeight;
+// const height = heroHeight - navbarHeight * 1.3;
+
+// window.addEventListener('scroll', (e) => {
+//   if (window.scrollY > height) {
+//     navbar.classList.replace('bg-transparent', 'bg-dark');
+//   }
+
+//   if (window.scrollY < height) {
+//     navbar.classList.replace('bg-dark', 'bg-transparent');
+//   }
+// });
+
+// Payment
+const packageList = document.querySelector('.bottom .list-group');
+const choosedPacakge = document.querySelector('.choosed-package');
+
+packageList.addEventListener('click', function (e) {
+  if (e.target.innerText == 'Package 1' || e.target.innerText == 'Package 2' || e.target.innerText == 'Package 3') {
+    // console.log(e.target.innerText);
+    choosedPacakge.innerHTML = `
+      ${e.target.innerText} <i onclick="closePayment()" class="fas fa-times-circle"></i>
+    `;
+  }
+});
+
+function closePayment() {
+  choosedPacakge.innerHTML = '';
+}
+
+// Ketika tombol payment di klik akan muncul alert, pembayaran belum tersedia
+const paymentBtn = document.querySelector('.payment-btn');
+
+paymentBtn.addEventListener('click', function () {
+  alert('Sorry, payment not yet available !');
+});
